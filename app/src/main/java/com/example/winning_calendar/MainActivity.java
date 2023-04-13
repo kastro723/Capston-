a76ab5c4b467051341e384e6b589d7580fb061bf
ksw62 <ksw6262@naver.com>
2023-04-13 16:56:10 +0900

    수정 3

--- app/src/main/java/com/example/winning_calendar/MainActivity.java
+++ app/src/main/java/com/example/winning_calendar/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {private FloatingActionButto
     CalendarLayout mLayout;
     CalendarView mView;
     DayView mDayView;
-       //수정 2
+       //수정 3
     enum state{
         CALENDAER,
         ADDDATE,
